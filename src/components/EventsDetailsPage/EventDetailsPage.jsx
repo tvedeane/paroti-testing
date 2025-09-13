79dfa7505e81bc156f2b80ad9e662e5605c66090
tvedeane <1138746+tvedeane@users.noreply.github.com>
2025-09-13 11:11:01 +0200

    Pass data to sidebar component

--- src/components/EventsDetailsPage/EventDetailsPage.jsx
+++ src/components/EventsDetailsPage/EventDetailsPage.jsx
@@ -7,36 +7,35 @@ import Sidebar from "./Sidebar";
 
 import Image from "../Image/Image";
 
-const { image, date, text3, texts, title, titleTwo } = eventDetailsPage;
+const { image, date, text3, texts, title, titleTwo, location } =
+  eventDetailsPage;
 
 const EventDetailsPage = () => {
-
-
   return (
-    <section className='sec-pad-top sec-pad-bottom events-details'>
+    <section className="sec-pad-top sec-pad-bottom events-details">
       <Container>
-        <div className='events-details__image'>
-          <Image relativePath={`events/${image}`} alt='' />
+        <div className="events-details__image">
+          <Image relativePath={`events/${image}`} alt="" />
 
-          <div className='events-card__date'>{date}</div>
+          <div className="events-card__date">{date}</div>
         </div>
-        <Row className='gutter-y-60'>
+        <Row className="gutter-y-60">
           <Col lg={8}>
-            <div className='events-details__content'>
-              <h3 className='events-card__title'>{title}</h3>
+            <div className="events-details__content">
+              <h3 className="events-card__title">{title}</h3>
               {texts.map((text, i) => (
                 <p key={i}>{text}</p>
               ))}
 
               <h4>{titleTwo}</h4>
               <p>{text3}</p>
-              <Link to='/contact' className='thm-btn events-details__btn'>
+              <Link to="/contact" className="thm-btn events-details__btn">
                 <span>Register your seat</span>
               </Link>
             </div>
           </Col>
           <Col lg={4}>
-            <Sidebar />
+            <Sidebar date={date} location={location} />
           </Col>
         </Row>
       </Container>
